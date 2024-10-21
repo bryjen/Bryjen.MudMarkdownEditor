a60ad8bb7f9ba8934863e60fe5aa1d9598677ddf
Bryan <miguelbryancarlo3434@gmail.com>
2024-10-20 23:42:34 -0400

    Initial Commit with sample github issues page

---
+++ Bryjen.MudMarkdownEditor.Sample/Components/Github/TimelineItem.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Bryjen.MudMarkdownEditor.Sample.Components.Github;
+
+public partial class TimelineItem : ComponentBase
+{
+    [Parameter]
+    public required Models.TimelineItem TimelineItemModel { get; set; }
+    
+    [Parameter]
+    public required RenderFragment AvatarRenderFragment { get; set; }
+    
+    [Parameter]
+    public bool IsHeaderItem { get; set; } = false;
+
+    [Parameter]
+    public bool IsAuthorIssueAuthor { get; set; } = false;
+    
+    [Parameter]
+    public bool IsAuthorRepoOwner { get; set; } = false;
+}
---
+++ Bryjen.MudMarkdownEditor.Sample/Models/TimelineItem.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Bryjen.MudMarkdownEditor.Sample.Models;
+
+public class TimelineItem
+{
+    public string ItemAuthor { get; init; } = string.Empty;
+    public DateTime PublishDate { get; init; } = DateTime.Now;
+    public string Contents { get; init; } = new(string.Empty);
+    public TimelineItem[] PreviousRevisions { get; set; } = [];
+
+    public bool IsRepoAuthor { get; init; } = false;
+}
