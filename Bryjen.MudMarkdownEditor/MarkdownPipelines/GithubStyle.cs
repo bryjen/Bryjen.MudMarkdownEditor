f705fd005037f3e00d2a4c6d13a301326edbc138
Bryan <miguelbryancarlo3434@gmail.com>
2024-10-26 19:58:38 -0400

    Adds a 'copy' button to each code block

--- Bryjen.MudMarkdownEditor/MarkdownPipelines/GithubStyle.cs
+++ Bryjen.MudMarkdownEditor/MarkdownPipelines/GithubStyle.cs
@@ -16,10 +16,14 @@ public class GithubStyle
         var syntaxHighlightedHtml = global::Markdig.Markdown.ToHtml(rawMarkdown, pipeline);
         
         // Further post-processing
+        
+        // We have to modify the html directly. Due to the implementation of 'Markdown.ColorCode', we can't modify the 
+        // markdown pipeline to change the elements included with the 'div' because the required classes are internal.
+        
         var htmlDoc = new HtmlDocument();
         htmlDoc.LoadHtml(syntaxHighlightedHtml);
         
-        AddScrollableCodeBlocks(ref htmlDoc);
+        ModifyCodeBlocks(ref htmlDoc);
 
         return $"""
                 <div class="markdown-body">
@@ -31,18 +35,36 @@ public class GithubStyle
                 """;
     }
 
-    private static void AddScrollableCodeBlocks(ref HtmlDocument htmlDocument)
+    private static void ModifyCodeBlocks(ref HtmlDocument htmlDocument)
     {
         var divNodes = htmlDocument.DocumentNode.SelectNodes("//div[not(@id) and count(*) = 1 and pre]");
 
         if (divNodes != null)
         {
+            int currentCodeBlock = 0;
+            
             foreach (var div in divNodes)
             {
-                div.SetAttributeValue("class", "scrollable-container");
+                if (div is not null)
+                {
+                    string baseId = $"code-block-{++currentCodeBlock}";
+                    
+                    div.SetAttributeValue("id", baseId);
+                    div.SetAttributeValue("class", "scrollable-container");
+                    div.AppendChild(HtmlNode.CreateNode(ClipboardButton($"{baseId}-clipboard-button")));
+                }
             }
         }
     }
+
+    private static string ClipboardButton(string id) =>
+        $"""
+        <div id="{id}" class="copy-to-clipboard-container">
+            <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" style="position: absolute; top: 25%; right: 25%; color: white; filter: brightness(0) invert(0.4);">
+                <path d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z"></path><path d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z"></path>
+            </svg>
+        </div>
+        """;
     
     
     private const string CssStyles = 
@@ -76,14 +98,50 @@ public class GithubStyle
             margin-left: 1.5rem;
         }
         
-        .markdown-body ul li::marker {
-            font-size: 1.2rem;
-            unicode-bidi: isolate;
-            font-variant-numeric: tabular-nums;
-            text-transform: none;
-            text-indent: 0px !important;
-            text-align: start !important;
-            text-align-last: start !important;
+        .markdown-body ul li.task-list-item::marker {
+        }
+        
+        
+        .scrollable-container {
+            position: relative;
+        
+            width: 100%;
+            white-space: pre;
+            overflow-x: auto;
+            box-sizing: border-box;
+            
+            border-radius: 0.25rem;
+            
+            padding: 1rem;
+            background-color: #262c36;
+        }
+        
+        
+        .copy-to-clipboard-container {
+            position: absolute; 
+            top: 0; 
+            right: 0; 
+            
+            width: 2rem; 
+            height: 2rem; 
+            margin: 0.5rem;
+            
+            background-color: #2a313c; 
+            border-radius: 0.4rem;
+            outline: 1px solid #3d444d;
+            
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+        
+        .copy-to-clipboard-container:hover {
+            background-color: #2f3742; 
+        }
+        
+        .copy-to-clipboard-container:active {
+            outline: 1px solid #337339; 
         }
         """;
 }
