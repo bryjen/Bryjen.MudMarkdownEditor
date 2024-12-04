999ff1f3d5658dc4552ca2a2055596405a3b2bf5
Bryan <miguelbryancarlo3434@gmail.com>
2024-12-04 10:35:16 -0500

    Makes 'base' 'dynamic' based on build config

---
+++ Bryjen.MudMarkdownEditor.Docs/SetBase.ps1
@@ -0,0 +1,16 @@
+﻿param (
+    [string]$BasePath
+)
+
+Write-Output "Executing: 'SetBase.ps1'"
+Write-Output "BasePath: $BasePath"
+Write-Output ""
+
+$indexTemplateFilePath = ".\wwwroot\index-template.html"
+$newIndexFileContents = (Get-Content $indexTemplateFilePath) -replace '%BASE_PATH%', $BasePath
+
+$outputFilePath = ".\wwwroot\index.html"
+Set-Content -Path $outputFilePath -Value $newIndexFileContents
+
+Write-Output "New '$outputFilePath' file contents:"
+Write-Output $newIndexFileContents
